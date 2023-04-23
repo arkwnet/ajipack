328fbc489b1d95d2000bd39467742763daa5f512
Sora Arakawa <mail@arkw.net>
2023-04-23 10:19:58 +0900

    studio: Install codemirror

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,3 +1,37 @@
 <template>
-  <div class="editor"></div>
+  <div class="editor">
+    <codemirror
+      v-model="code"
+      :style="{ height: '400px' }"
+      :autofocus="true"
+      :indent-with-tab="true"
+      :tab-size="2"
+      :extensions="extensions"
+      @ready="handleReady"
+    />
+  </div>
 </template>
+
+<script>
+import { defineComponent, shallowRef } from "vue";
+import { Codemirror } from "vue-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+
+export default defineComponent({
+  components: {
+    Codemirror,
+  },
+  setup() {
+    const extensions = [javascript()];
+    const view = shallowRef();
+    const handleReady = (payload) => {
+      view.value = payload.view;
+    };
+    return {
+      extensions,
+      handleReady,
+      log: console.log,
+    };
+  },
+});
+</script>
